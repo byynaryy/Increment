fc87f9b9e73965f3043af36a193edf9a39b219f6
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-10-14 13:52:16 +0200

    add comments

--- src/Increment.java
+++ src/Increment.java
@@ -1,18 +1,18 @@
 public class Increment {
     public static void main(String[] args) {
         int x = 4;
-        // postfix
         System.out.println("Initial value of x: " + x);
         // Useful info: x=x+1 is the same as x+=1 is the same as x++
+        // postfix
         // Post-increment: Use the current value of x and then increment
-        System.out.println("Post-increment (x++): " + x++);
-        System.out.println("Value of x after post-increment: " + x);
+        System.out.println("Post-increment (x++): " + x++); // Postfix (x++): 4
+        System.out.println("Value of x after post-increment: " + x); // Value of x after post-increment: 5
 
         // Reset x to 4
         x = 4;
         // prefix
         // Pre-increment: Increment x and then use the updated value
-        System.out.println("Pre-increment (++x): " + ++x);
-        System.out.println("Value of x after pre-increment: " + x);
+        System.out.println("Pre-increment (++x): " + ++x); // Prefix (++x): 5
+        System.out.println("Value of x after pre-increment: " + x); // Value of x after pre-increment: 5
     }
 }
